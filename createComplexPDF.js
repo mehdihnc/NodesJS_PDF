9b4125de117d666eb36ee39811bd264fccf67dc6
mehdihnc <mehdiharchenif@gmail.com>
2023-11-21 14:34:10 +0100

    change comments

--- createComplexPDF.js
+++ createComplexPDF.js
@@ -1,42 +1,46 @@
 // Importation des bibliothèques nécessaires
-const PDFDocument = require('pdfkit'); // pdfkit est la bibliothèque utilisée pour créer des documents PDF.
-const fs = require('fs'); // 'fs' (FileSystem) est utilisé pour interagir avec le système de fichiers, ici pour enregistrer le PDF.
+// pdfkit est utilisé pour la création du document PDF, et fs (File System) est nécessaire pour sauvegarder le fichier PDF sur le disque.
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 
-// Création d'un nouveau document PDF
-let doc = new PDFDocument(); // Initialise un nouveau document PDF.
+// Initialisation d'un nouveau document PDF
+// Ici, un nouveau document PDF est créé. C'est le point de départ pour toute création de contenu dans le PDF.
+let doc = new PDFDocument();
 
-// Configuration du flux de sortie pour écrire le PDF dans un fichier
-doc.pipe(fs.createWriteStream('complexDocument.pdf')); // Crée un flux de sortie pour enregistrer le document PDF en tant que 'complexDocument.pdf'.
+// Configuration du fichier de sortie pour le document PDF
+// Cette ligne configure le document pour qu'il soit écrit dans un fichier nommé 'complexDocument.pdf' sur votre système.
+doc.pipe(fs.createWriteStream('complexDocument.pdf'));
 
-// Ajout de texte formaté dans le document
-doc.font('Times-Roman') // Définit la police de caractères pour le texte suivant.
-   .fontSize(24) // Définit la taille de la police pour le texte suivant.
-   .fillColor('blue') // Définit la couleur du texte.
-   .text('Bienvenue dans votre PDF!', 100, 100); // Ajoute le texte au document avec la position spécifiée (x: 100, y: 100).
+// Ajout de texte formaté au document
+// Dans cette section, nous ajoutons du texte au document. Nous définissons la police, la taille, la couleur et le contenu du texte, ainsi que sa position dans le document.
+doc.font('Times-Roman').fontSize(24).fillColor('blue').text('Bienvenue dans votre PDF!', 100, 100);
 
-// Insertion d'une image
-doc.image('images/chat.jpg', { // Spécifie le chemin de l'image et les options.
-     fit: [250, 300], // Adapte la taille de l'image à ces dimensions (largeur: 250, hauteur: 300).
-     align: 'center', // Centre l'image horizontalement dans le document.
-     valign: 'center' // Centre l'image verticalement dans le document.
+// Insertion d'une image dans le document
+// Ce bloc ajoute une image au PDF. Vous devez fournir le chemin de l'image et vous pouvez également spécifier sa taille et sa position.
+doc.image('images/chat.jpg', {
+    fit: [250, 300],
+    align: 'center',
+    valign: 'center'
 });
 
 // Ajout d'une liste de tâches
-doc.moveDown(); // Déplace le curseur vers le bas pour séparer les sections.
-doc.fontSize(14).fillColor('white'); // Réinitialise la taille de la police et la couleur pour la liste.
-doc.text('Liste de tâches :', { underline: true }); // Ajoute un titre pour la liste avec un soulignement.
-const tasks = ['Tâche 1', 'Tâche 2', 'Tâche 3']; // Définit un tableau de tâches.
-tasks.forEach((task, index) => { // Itère sur chaque tâche et l'ajoute au document.
-    doc.text(`${index + 1}. ${task}`); // Formatte et ajoute chaque tâche.
+// Ici, nous ajoutons une liste de tâches au document. Chaque tâche est ajoutée individuellement avec un numéro d'ordre.
+doc.moveDown();
+doc.fontSize(14).fillColor('white').text('Liste de tâches :', { underline: true });
+const tasks = ['Tâche 1', 'Tâche 2', 'Tâche 3'];
+tasks.forEach((task, index) => {
+    doc.text(`${index + 1}. ${task}`);
 });
 
-// Création d'un tableau simple
-doc.moveDown(); // Déplace le curseur vers le bas.
-doc.text('Emploi du temps :', { underline: true }); // Ajoute un titre pour l'emploi du temps.
-const timetable = [['Lundi', 'Cours de NodeJS'], ['Mardi', 'Cours de PDFKit']]; // Définit un tableau pour l'emploi du temps.
-timetable.forEach(row => { // Itère sur chaque ligne de l'emploi du temps et l'ajoute au document.
-    doc.text(`${row[0]}: ${row[1]}`); // Formatte et ajoute chaque ligne de l'emploi du temps.
+// Création et ajout d'un tableau simple au document
+// Cette section crée un tableau simple sous forme de texte. Chaque ligne du tableau est ajoutée séparément.
+doc.moveDown();
+doc.text('Emploi du temps :', { underline: true });
+const timetable = [['Lundi', 'Cours de NodeJS'], ['Mardi', 'Cours de PDFKit']];
+timetable.forEach(row => {
+    doc.text(`${row[0]}: ${row[1]}`);
 });
 
-// Finalisation du document
-doc.end(); // Indique la fin du document PDF. Après cette ligne, plus rien ne peut être ajouté au document.
+// Finalisation du document PDF
+// Cette ligne marque la fin du document. Après cette commande, plus aucun contenu ne peut être ajouté au PDF.
+doc.end();
