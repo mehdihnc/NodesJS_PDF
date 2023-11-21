a0b734171e0c71504680729cc03593e3ac2f54c4
mehdihnc <mehdiharchenif@gmail.com>
2023-11-21 14:41:41 +0100

    change comments

--- createComplexPDF.js
+++ createComplexPDF.js
@@ -1,30 +1,18 @@
-// Importation des bibliothèques nécessaires
-// pdfkit est utilisé pour la création du document PDF, et fs (File System) est nécessaire pour sauvegarder le fichier PDF sur le disque.
+// Importation de 'pdfkit' pour la création de documents PDF et de 'fs' pour écrire le fichier PDF sur le disque.
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
-// Initialisation d'un nouveau document PDF
-// Ici, un nouveau document PDF est créé. C'est le point de départ pour toute création de contenu dans le PDF.
+// Initialisation d'un document PDF et configuration de son flux de sortie pour enregistrer dans 'complexDocument.pdf'.
 let doc = new PDFDocument();
-
-// Configuration du fichier de sortie pour le document PDF
-// Cette ligne configure le document pour qu'il soit écrit dans un fichier nommé 'complexDocument.pdf' sur votre système.
 doc.pipe(fs.createWriteStream('complexDocument.pdf'));
 
-// Ajout de texte formaté au document
-// Dans cette section, nous ajoutons du texte au document. Nous définissons la police, la taille, la couleur et le contenu du texte, ainsi que sa position dans le document.
+// Ajout de texte formaté au PDF, en spécifiant la police, la taille, la couleur et la position.
 doc.font('Times-Roman').fontSize(24).fillColor('blue').text('Bienvenue dans votre PDF!', 100, 100);
 
-// Insertion d'une image dans le document
-// Ce bloc ajoute une image au PDF. Vous devez fournir le chemin de l'image et vous pouvez également spécifier sa taille et sa position.
-doc.image('images/chat.jpg', {
-    fit: [250, 300],
-    align: 'center',
-    valign: 'center'
-});
+// Insertion d'une image en définissant son chemin, sa taille ajustée et son positionnement dans le document.
+doc.image('images/chat.jpg', {fit: [250, 300], align: 'center', valign: 'center'});
 
-// Ajout d'une liste de tâches
-// Ici, nous ajoutons une liste de tâches au document. Chaque tâche est ajoutée individuellement avec un numéro d'ordre.
+// Ajout d'une liste de tâches, en utilisant une boucle pour générer chaque item de la liste.
 doc.moveDown();
 doc.fontSize(14).fillColor('white').text('Liste de tâches :', { underline: true });
 const tasks = ['Tâche 1', 'Tâche 2', 'Tâche 3'];
@@ -32,8 +20,7 @@ tasks.forEach((task, index) => {
     doc.text(`${index + 1}. ${task}`);
 });
 
-// Création et ajout d'un tableau simple au document
-// Cette section crée un tableau simple sous forme de texte. Chaque ligne du tableau est ajoutée séparément.
+// Création d'un tableau simple pour l'emploi du temps, en affichant chaque jour et activité associée.
 doc.moveDown();
 doc.text('Emploi du temps :', { underline: true });
 const timetable = [['Lundi', 'Cours de NodeJS'], ['Mardi', 'Cours de PDFKit']];
@@ -41,6 +28,5 @@ timetable.forEach(row => {
     doc.text(`${row[0]}: ${row[1]}`);
 });
 
-// Finalisation du document PDF
-// Cette ligne marque la fin du document. Après cette commande, plus aucun contenu ne peut être ajouté au PDF.
+// Finalisation du document, indiquant que le PDF est complet et prêt à être sauvegardé.
 doc.end();
